2ee844759cf1fa8f98a42bfac6419a807ac50d1a
jnathan0222 <123518866+jnathan0222@users.noreply.github.com>
2024-01-31 16:09:42 -0500

    Created download button that converts Nodes to CSV

--- map-app/src/Components/Node.js
+++ map-app/src/Components/Node.js
@@ -5,7 +5,17 @@ class Node {
         this.altitude = altitude;
         this.name = name;
         this.neighbours = [];
+        
+    }
+
+    toString(){
+        let string = this.name + "," + this.latitude + "," + this.longitude + "," + this.altitude + ",";
+        for(let i = 0; i < this.neighbours.length; i++){
+            string += this.neighbours[i].name + ",";
+        }
+        return string;
     }
 }
+
 
 export default Node;
